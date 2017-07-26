58133c956655441c46a634b993b0bca9c61ea135
Matthew Mitrik <mmitrik@gmail.com>
2017-07-26 15:11:46 -0400

    Adding a POST handler.

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
+})
+
+app.post('/', function (req, res) {
+    res.send('Received the POST')
 })
